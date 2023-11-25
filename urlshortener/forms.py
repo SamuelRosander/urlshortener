ff561bc7fc7d6e391d056119734b4ccc39e1051f
Samuel Rosander <s.rosander@gmail.com>
2023-11-25 12:15:30 +0100

    Refactor

---
+++ urlshortener/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+from wtforms.fields import URLField
+from wtforms.validators import DataRequired
+
+class LinkForm(FlaskForm):
+    long_url = URLField("Long URL", validators=[DataRequired()])
+    submit = SubmitField("Shorten")
